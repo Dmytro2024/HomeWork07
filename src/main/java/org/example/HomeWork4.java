e38b055fc9b226f54fee37e450ec46738c6690ae
Dmytro2024 <microzo@hotmail.com>
2024-02-11 12:46:41 +0200

    My First Commit 11022024_1200

--- src/main/java/org/example/HomeWork4.java
+++ src/main/java/org/example/HomeWork4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class HomeWork4 {
 
-    public static  void main(String[] args) {
+    public static <Scanner> void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input First Number: " );
