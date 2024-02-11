de4943d7eb834a62284c3d107fe9c00a332fcfb2
Dmytro2024 <microzo@hotmail.com>
2024-02-11 12:12:48 +0200

    My First Commit 11022024_1200

---
+++ src/main/java/org/example/HomeWork4.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class HomeWork4 {
+
+    public static  void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input First Number: " );
+        float a = scanner.nextFloat();
+        System.out.print("What is operation (+, -, *, /,): " );
+        char operation = scanner.next().charAt(0);
+        System.out.print("Input Second Number: " );
+        float b = scanner.nextFloat();
+
+        if  (operation == '+') {
+            float result = a + b;
+            System.out.println(+a + " + " + b + " = " + result);
+        } else if (operation == '-') {
+            float result = a - b;
+            System.out.println(+a + " - " + b + " = " + result);
+        }
+        else if  (operation == '*') {
+            float result = a * b;
+            System.out.println(+a + " * " + b + " = " + result);
+        }
+        else if  (operation == '/') {
+            float result = a / b;
+            System.out.println(+a + " / " + b + " = " + result);
+        } else {
+            System.out.println("Not correct!");
+
+        }
+    }
+
+}
+
+
