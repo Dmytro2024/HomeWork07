4cd3980ca6b0297809e086775d8b58e539fc10d9
Dmytro2024 <microzo@hotmail.com>
2024-02-14 12:00:04 +0200

    Add new Metod prepareResults

--- src/main/java/org/example/HomeWork4.java
+++ src/main/java/org/example/HomeWork4.java
@@ -2,38 +2,38 @@ package org.example;
 
 import java.util.Scanner;
 
-public class HomeWork4 {
-
-    public static <Scanner> void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input First Number: " );
-        float a = scanner.nextFloat();
-        System.out.print("What is operation (+, -, *, /,): " );
-        char operation = scanner.next().charAt(0);
-        System.out.print("Input Second Number: " );
-        float b = scanner.nextFloat();
-
-        if  (operation == '+') {
-            float result = a + b;
-            System.out.println(+a + " + " + b + " = " + result);
-        } else if (operation == '-') {
-            float result = a - b;
-            System.out.println(+a + " - " + b + " = " + result);
-        }
-        else if  (operation == '*') {
-            float result = a * b;
-            System.out.println(+a + " * " + b + " = " + result);
-        }
-        else if  (operation == '/') {
-            float result = a / b;
-            System.out.println(+a + " / " + b + " = " + result);
-        } else {
-            System.out.println("Not correct!");
-
-        }
-    }
-
-}
+//public class HomeWork4 {
+//
+//    public static <Scanner> void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Input First Number: " );
+//        float a = scanner.nextFloat();
+//        System.out.print("What is operation (+, -, *, /,): " );
+//        char operation = scanner.next().charAt(0);
+//        System.out.print("Input Second Number: " );
+//        float b = scanner.nextFloat();
+//
+//        if  (operation == '+') {
+//            float result = a + b;
+//            System.out.println(+a + " + " + b + " = " + result);
+//        } else if (operation == '-') {
+//            float result = a - b;
+//            System.out.println(+a + " - " + b + " = " + result);
+//        }
+//        else if  (operation == '*') {
+//            float result = a * b;
+//            System.out.println(+a + " * " + b + " = " + result);
+//        }
+//        else if  (operation == '/') {
+//            float result = a / b;
+//            System.out.println(+a + " / " + b + " = " + result);
+//        } else {
+//            System.out.println("Not correct!");
+//
+//        }
+//    }
+//
+//}
 
 
---
+++ src/main/java/org/example/prepareResults.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.List;
+public class prepareResults {
+
+
+    public class Main {
+        public static void main(String[] args) {
+            List<String> inputData = List.of("5", "+", "-5"); // Приклад вхідних даних
+            double result = 0.0; // Припустимо, результат розрахунку
+
+            // Виклик методу prepareResults
+            String output = prepareResults(inputData, result);
+            System.out.println(output); // Виведення результату
+        }
+
+        public static String prepareResults(List<String> inputData, double result) {
+            StringBuilder builder = new StringBuilder();
+            for (String data : inputData) {
+                builder.append(data);
+            }
+            builder.append("=").append(result);
+            return builder.toString();
+        }
+    }
+
+}
